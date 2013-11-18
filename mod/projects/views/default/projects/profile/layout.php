062e34a25265a43a301512232d30149780e3225b
aleph1888 <aleph1888@riseup.net>
2013-11-18 20:17:16 +0100

    Change view order between widgets and summary.

--- mod/projects/views/default/projects/profile/layout.php
+++ mod/projects/views/default/projects/profile/layout.php
@@ -8,5 +8,9 @@
  * @uses $vars['entity']
  */
 
+elgg_load_library ('elgg:fundcampaigns');
+if (fundcampaigns_get_active_campaign ($vars['entity']->guid)) {
+	echo elgg_view('projects/profile/widgets', $vars);
+}
 echo elgg_view('projects/profile/summary', $vars);
-echo elgg_view('projects/profile/widgets', $vars);
+
